bd34148caf90356d21bb47c45e1cbcc6c98ef991
Muhammad Shahpar Nafees Khan <66970201+shahparkhan@users.noreply.github.com>
2023-04-02 21:10:40 -0400

    Removed debug statements

--- src/create_vuln_reports.py
+++ src/create_vuln_reports.py
@@ -8,7 +8,6 @@ from time import sleep
 TOP_DIR = "/home/s33khan/Documents/woc_project/vulnerable-python-packages-study/output/requirements"
 CSV_PATH = "/home/s33khan/Documents/woc_project/vulnerable-python-packages-study/output/data.csv"
 HEADER = ["Project","Author","Time","Timezone","Scanned Packages","Packages Count", "Vulnerabilities Count", "Vulnerability ID","Vulnerable Package","Vulnerable Spec","Analyzed Version","CVE"]
-# HEADER = ["Project","Author","Time","Timezone","Scanned Packages","Packages Count", "Vulnerabilities Count", "Vulnerability Info"]
 
 def escape_file_path(filepath):
 
@@ -19,8 +18,6 @@ def escape_file_path(filepath):
     filepath = filepath.replace("(", "\\(")
     filepath = filepath.replace("'", "\\'")
 
-    # "1409143463_+0300_Theodoros Giannakopoulos <tyiannak@gmail.com>"
-    # "1409143463_+0300_Theodoros_Giannakopoulos_tyiannak@gmail.com"
 
     return filepath
 
@@ -46,12 +43,10 @@ def addRowToCSV(row):
 def main():
     global TOP_DIR, HEADER
 
-    # print(os.listdir(TOP_DIR))
 
     # Extracting project names
     project_names = os.listdir(TOP_DIR)
 
-    # print(os.listdir(TOP_DIR + "/" + all_project_names[0]))
 
     count = 0
     non_empty = 0
@@ -60,13 +55,7 @@ def main():
 
     for project_name in project_names:
         
-        # print(str(count) + " " + project_name)
-        # count = count + 1
-        # continue
 
-        if count < 23:
-            count = count + 1
-            continue
 
         commit_infos = os.listdir(TOP_DIR + "/" + project_name)
 
@@ -95,14 +84,10 @@ def main():
                     print("commit info: " + commit_info)
                     sleep(10)
 
-            # print(output)
-            # print("project name: " + project_name)
-            # print("commit info: " + commit_info)
             scanned_packages = get_scanned_packages_with_verisons(obj["scanned_packages"])
             packages_count = obj["report_meta"]["packages_found"]
             vulnerabilities_count = obj["report_meta"]["vulnerabilities_found"]
 
-            # vuln_info = ""
 
             for vuln in obj["vulnerabilities"]:
                 vuln_id = vuln["vulnerability_id"]
@@ -110,25 +95,12 @@ def main():
                 vuln_spec = vuln["vulnerable_spec"]
                 analyzed_version = vuln["analyzed_version"]
                 cve = vuln["CVE"]
-                # advisory = vuln["advisory"].replace("\n", "")
-                # advisory = advisory.replace("\r", "")
-
-                # if vuln_info != "":
-                #     vuln_info = vuln_info + ":"
-
-                # vuln_info = vuln_info + str(vuln_id) + "_" + str(vuln_package) + "_" + str(vuln_spec) + "_" + str(analyzed_version) + "_" + str(cve)
 
                 row = [project_name, author, time, timezone, scanned_packages, packages_count, vulnerabilities_count, vuln_id, vuln_package, vuln_spec, analyzed_version, cve]
 
                 addRowToCSV(row)
 
             if int(vulnerabilities_count) == 0:
-                # vuln_id = "NA"
-                # vuln_package = "NA"
-                # vuln_spec = "NA"
-                # analyzed_version = "NA"
-                # cve = "NA"
-                # advisory = "NA"
                 row = [project_name, author, time, timezone, scanned_packages, packages_count, vulnerabilities_count]
 
                 addRowToCSV(row)
@@ -140,34 +112,10 @@ def main():
         print("Project " + str(count) + " Done...")
         print("Project Name: " + project_name)
         count = count + 1
-        # print("scanned_packages: " + get_scanned_packages_with_verisons(obj["scanned_packages"]))
 
 
     
-    # print("empty: ", empty)
-    # print("non_empty: ", non_empty)
-
-    # project_0 = os.listdir(TOP_DIR + "/" + all_project_names[0])
-
-    # print(TOP_DIR + "/" + all_project_names[0] + "/" + project_0[0])
-    # print(os.path.isfile(TOP_DIR + "/" + all_project_names[0] + "/" + project_0[0]))
-    # print(TOP_DIR + "/" + all_project_names[0] + "/" + escape_file_path(project_0[0]))
-    # print(os.path.isfile(TOP_DIR + "/" + all_project_names[0] + "/" + escape_file_path(project_0[0])))
-
     
-
-    # print(subprocess.check_output("cat " + TOP_DIR + "/" + all_project_names[0] + "/" + "1409143463_+0300_Theodoros_Giannakopoulos_tyiannak@gmail.com.txt | safety check --stdin", stderr=subprocess.STDOUT, stdin=subprocess.PIPE, shell=True).decode("utf-8"))
-
-    # stream = os.popen("cat " + TOP_DIR + "/" + all_project_names[0] + "/" + escape_file_path(project_0[0]) + " | safety check --stdin --output json")
-    # output = stream.read()
-    # print(output)
-
-    # obj = json.loads(output)
-
-    # print(obj["scanned_packages"])
-
-    # print(type(output))
-
 
 if __name__ == "__main__":
     try:
